fbb6ecc2173c306c8ed5520ba32e7273cd46e278
navegador <navegador@05000219-70882>
2019-10-24 23:30:34 -0300

    Commit: +Response +Http

---
+++ Services/Safari.Services.Http/SpecieServiceHttp.cs
@@ -0,0 +1,60 @@
+﻿using Safari.Business;
+using Safari.Services.Contracts.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Safari.Services.Http
+{
+    [RoutePrefix("api/specie")]
+    public class SpecieServiceHttp : ApiController
+    {
+        [HttpPost]
+        [Route("addSpecie")]
+        public AddSpecieResponse add()
+        {
+            try
+            {
+                return null;
+            }
+            catch (Exception ex)
+            {
+                var httpError = new HttpResponseMessage()
+                {
+                    StatusCode = (HttpStatusCode)422,
+                    ReasonPhrase = ex.Message
+                };
+                throw new HttpResponseException(httpError);
+            }
+        }
+
+        [HttpGet]
+        [Route("getAll")]
+        public  AllSpeciesResponse getAll()
+        {
+            try
+            {
+                var response = new AllSpeciesResponse();
+                var bc = new SpecieComponent();
+                response.Result = bc.List();
+
+                return response;
+                
+            }
+            catch (Exception ex)
+            {
+                var httpError = new HttpResponseMessage()
+                {
+                    StatusCode = (HttpStatusCode)422,
+                    ReasonPhrase = ex.Message
+                };
+                throw new HttpResponseException(httpError);
+            }
+        }
+    }
+}
